44e6ec4ce06dacd6f6cdfac30997940abed0b8de
Vladimir <anikeev30@yandex.ru>
2024-12-09 03:42:09 +0900

    add database.sql

---
+++ database.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS urls CASCADE;
+
+CREATE TABLE urls (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS url_checks CASCADE;
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id BIGINT REFERENCES urls (id),
+    status_code INTEGER,
+    h1 TEXT,
+    title TEXT,
+    description TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
