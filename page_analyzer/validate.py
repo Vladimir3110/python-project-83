a3d224a84be2d7db616a2622c3777444ba24e33e
Vladimir <anikeev30@yandex.ru>
2024-12-14 18:42:18 +0900

    изменено: normalize_url

--- page_analyzer/validate.py
+++ page_analyzer/validate.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urlunparse
 
 import validators
 from flask import flash
@@ -15,6 +15,21 @@ def validate_url(url):
 def normalize_url(url):
     """Нормализация URL, добавление схемы, если она отсутствует."""
     parsed_url = urlparse(url)
+
     if not parsed_url.scheme:
         url = 'http://' + url
-    return url.lower()
+        parsed_url = urlparse(url)  # Повторный разбор после добавления схемы
+    # Нормализация компонентов URL
+    scheme = parsed_url.scheme.lower()
+    netloc = parsed_url.netloc.lower()
+    path = parsed_url.path.lower().rstrip('/')
+    query = parsed_url.query
+    fragment = parsed_url.fragment
+    # Удаление стандартных портов
+    if scheme == 'http' and netloc.endswith(':80'):
+        netloc = netloc[:-3]  # Удаление ':80'
+    elif scheme == 'https' and netloc.endswith(':443'):
+        netloc = netloc[:-4]  # Удаление ':443'
+    # Восстановление нормализованного URL
+    normalized_url = urlunparse((scheme, netloc, path, '', query, fragment))
+    return normalized_url
