27ed4fbe965f704589a2ce4d0f79f202a9025597
Vladimir <anikeev30@yandex.ru>
2024-12-21 03:26:06 +0900

    изменено: def check_seo(url):

--- page_analyzer/parser.py
+++ page_analyzer/parser.py
@@ -9,38 +9,9 @@ headers = {
 }
 
 
-# def check_seo(url):
-#    try:
-#        response = requests.get(url, headers=headers, timeout=20)
-#        response.raise_for_status()  # Проверка на ошибки HTTP
-#        # Выводим HTML-код страницы в консоль для отладки
-#        # print(response.text)
-
-#        soup = BeautifulSoup(response.text, 'html.parser')  # html.parser
-#        title = soup.title.string if soup.title else 'Нет заголовка'
-#        description = soup.find('meta', attrs={'name': 'description'})
-#       description = description['content'] if description else 'Нет описания'
-
-#        return {
-#            'title': title,
-#            'description': description,
-#            'status_code': response.status_code
-#        }
-#    except Exception as e:
-#        # ошибка в консоль для отладки
-#        # print(f"Ошибка при проверке SEO: {e}")
-#        # Возвращаем None, если произошла ошибка
-#        return {
-#            'status_code': None,
-#            'title': None,
-#            'description': None,
-#            'error': str(e)
-#        }
-
-
 def check_seo(url):
     try:
-        response = requests.get(url, headers=headers, timeout=20)
+        response = requests.get(url, headers=headers, timeout=0)
         response.raise_for_status()  # Проверка на ошибки HTTP
         # Выводим HTML-код страницы в консоль для отладки
         # print(response.text)
