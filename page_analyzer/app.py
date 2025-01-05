83379fbca54d89b114935d04df05e18cbd12377d
Vladimir <anikeev30@yandex.ru>
2025-01-06 01:41:04 +0900

    изменено: контекстный менеджер в def check_url():, def handle_check_url():

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,3 +1,4 @@
+from contextlib import closing
 from datetime import datetime
 
 import psycopg2
@@ -92,14 +93,12 @@ def check_url(id):
     """Обработчик маршрута для проверки URL."""
     conn = None
     try:
-        conn = psycopg2.connect(DATABASE_URL)
-        return handle_check_url(conn, id)
+        with psycopg2.connect(DATABASE_URL) as conn:
+            with closing(conn.cursor()) as cursor:
+                return handle_check_url(cursor, id)
     except Exception as e:
         flash(f'Ошибка при подключении к базе данных: {e}', 'error')
         return redirect(url_for('urls'))
-    finally:
-        if conn:
-            conn.close()
 
 
 if __name__ == '__main__':
--- page_analyzer/url_check.py
+++ page_analyzer/url_check.py
@@ -1,3 +1,4 @@
+from contextlib import closing
 from datetime import datetime
 
 import psycopg2
@@ -13,41 +14,37 @@ def handle_check_url(conn, id):
     formatted_check_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     cursor = None
     try:
-        cursor = conn.cursor()
-        cursor.execute('SELECT name FROM urls WHERE id = %s', (id,))
-        url = cursor.fetchone()
-        if not url:
-            flash('URL не найден', 'error')
-            return redirect(url_for('list_urls'))
+        with closing(conn.cursor()) as cursor:
+            cursor.execute('SELECT name FROM urls WHERE id = %s', (id,))
+            url = cursor.fetchone()
+            if not url:
+                flash('URL не найден', 'error')
+                return redirect(url_for('list_urls'))
 
-        errors = validate_url(url[0])
-        if errors:
-            flash('Некорректный URL', 'error')
-            return redirect(url_for('show_url', id=id))
+            errors = validate_url(url[0])
+            if errors:
+                flash('Некорректный URL', 'error')
+                return redirect(url_for('show_url', id=id))
 
-        normalized_url = normalize_url(url[0])
+            normalized_url = normalize_url(url[0])
 
         # Получаем SEO данные
-        seo_data = check_seo(normalized_url)
-        if seo_data is None or seo_data.get('status_code') != 200:
-            flash('Произошла ошибка при проверке', 'error')
-            return redirect(url_for('show_url', id=id))
+            seo_data = check_seo(normalized_url)
+            if seo_data is None or seo_data.get('status_code') != 200:
+                flash('Произошла ошибка при проверке', 'error')
+                return redirect(url_for('show_url', id=id))
 
-        title = seo_data.get('title', 'Нет заголовка')
-        description = seo_data.get('description', 'Нет описания')
-        h1 = seo_data.get('h1', 'Нет h1')
+            title = seo_data.get('title', 'Нет заголовка')
+            description = seo_data.get('description', 'Нет описания')
+            h1 = seo_data.get('h1', 'Нет h1')
 
-        insert_url_check(id, seo_data.get('status_code'), title,
-                         description, h1, formatted_check_date)
-        flash('Страница успешно проверена!', 'success')
+            insert_url_check(id, seo_data.get('status_code'), title,
+                             description, h1, formatted_check_date)
+            flash('Страница успешно проверена!', 'success')
     except psycopg2.OperationalError as e:
         print(f'Невозможно установить соединение с базой данных: {e}')
         flash('Ошибка подключения к базе данных', 'error')
     except Exception as e:
         flash(f'Ошибка при добавлении проверки: {e}', 'error')
-    finally:
-        if cursor:
-            cursor.close()
-        if conn:
-            conn.close()
+
     return redirect(url_for('show_url', id=id))
